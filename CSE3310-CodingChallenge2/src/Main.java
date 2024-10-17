f08a3fa82ce76a60b4f036638b62e70c03a3398e
alwolfeCBE <83241214+alwolfeCBE@users.noreply.github.com>
2024-10-17 08:56:09 -0600

    Add files via upload

---
+++ CSE3310-CodingChallenge2/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+    public static void reverseDisplayLoop(int value){
+
+    }
+    public static void reverseDisplayRecursion(int value){
+
+    }
+    public static void reverseDisplayLoop(String value){
+
+    }
+    public static void reverseDisplayRecursion(String value){
+        
+    }
+}
